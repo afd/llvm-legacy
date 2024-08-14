d1d68ebe776433a2068483fe398fa5c5661fb2fa
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2024-08-14 18:53:54 +0100

    Fix: update lit and script to use python 2.7

--- llvm-2.8/test/Scripts/coff-dump.py
+++ llvm-2.8/test/Scripts/coff-dump.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 #===-- coff-dump.py - COFF object file dump utility-------------------------===#
 #
 #                     The LLVM Compiler Infrastructure
--- llvm-2.8/tools/clang/utils/CIndex/completion_logger_server.py
+++ llvm-2.8/tools/clang/utils/CIndex/completion_logger_server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 import sys
 from socket import *
 from time import strftime
