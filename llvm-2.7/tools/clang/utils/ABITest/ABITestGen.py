d89394dee23be72afa3f6e918168f70a6de90b17
JonathanFoo0523 <jonathanfoo0523@gmail.com>
2024-08-17 04:15:13 +0000

    Use python2.7 for all scripts

--- llvm-2.7/tools/clang/utils/ABITest/ABITestGen.py
+++ llvm-2.7/tools/clang/utils/ABITest/ABITestGen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 
 from pprint import pprint
 import random, atexit, time
--- llvm-2.7/tools/clang/utils/TestUtils/deep-stack.py
+++ llvm-2.7/tools/clang/utils/TestUtils/deep-stack.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 
 def pcall(f, N):
     if N == 0:
